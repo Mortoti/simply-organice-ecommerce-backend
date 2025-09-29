5680e9c6b9c6507420ddea9b9fd1871e775a842b
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-09-29 16:35:22 +0000

    Product Model Updated - Added upload_to for Images

--- store/models.py
+++ store/models.py
@@ -9,7 +9,7 @@ class Collection(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='products/')
     price = models.DecimalField(decimal_places=2, max_digits=10)
     availability = models.BooleanField()
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
