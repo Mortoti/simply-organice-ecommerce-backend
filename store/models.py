b918d09a9dced47f6386686a758ab13b034defc9
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-09-29 16:08:11 +0000

    Product Model Updated

--- store/models.py
+++ store/models.py
@@ -8,3 +8,8 @@ class Collection(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    image = models.ImageField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    availability = models.BooleanField()
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+
