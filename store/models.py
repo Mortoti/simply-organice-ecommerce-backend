150369f85c50064ff8c72470ec31dde91a36654e
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-09-29 16:03:16 +0000

    Product Model Created

--- store/models.py
+++ store/models.py
@@ -5,3 +5,6 @@ class Branch(models.Model):
     name = models.CharField(max_length=100)
 class Collection(models.Model):
     name = models.CharField(max_length=100)
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
