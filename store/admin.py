6bd82b198ad165f04e70fe41431e4816dca6b92c
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-09-30 12:21:29 +0000

    Branch Model Registered in Admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+@admin.register(models.Branch)
+class BranchAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active')
+    list_editable = ('is_active',)
+    search_fields = ('name',)
+    ordering = ('name',)
+    list_filter = ('is_active',)
