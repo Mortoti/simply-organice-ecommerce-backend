6797f70965c5299575f4febb4bed40ad7e5da8c3
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-09-30 21:37:05 +0000

    istartswith Added to all search fields

--- store/admin.py
+++ store/admin.py
@@ -5,21 +5,21 @@ from . import models
 class BranchAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_active')
     list_editable = ('is_active',)
-    search_fields = ('name',)
+    search_fields = ('name__istartswith',)
     ordering = ('name',)
     list_filter = ('is_active',)
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ("name",)
     list_filter = ('name',)
-    search_fields = ('name',)
+    search_fields = ('name__istartswith',)
     list_per_page = 10
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'is_available')
     list_filter = ('is_available',)
     autocomplete_fields = ('collection',)
-    search_fields = ('name',)
+    search_fields = ('name__istartswith',)
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('customer_name',
@@ -31,6 +31,8 @@ class OrderAdmin(admin.ModelAdmin):
     autocomplete_fields = ('branch',)
     list_filter = ('status',)
     search_fields = ('customer_name__istartswith','recipient_name__istartswith',)
+    list_editable = ('status',)
+
 
 @admin.register(models.OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
