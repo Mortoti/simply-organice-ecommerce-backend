843e47a80f4d47063512beacbc5b514e8dc5ab0a
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-10-10 11:34:06 +0000

    Implementation of UUID for Cart id

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
 class Branch(models.Model):
@@ -58,10 +59,9 @@ class OrderItem(models.Model):
     def __str__(self):
         return f'Order {self.pk} - {self.product.name}'
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
-    client_token = models.CharField(max_length=100, unique=True)
-    def __str__(self):
-        return f'Cart {self.client_token}'
+
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
