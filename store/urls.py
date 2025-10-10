11e250613585ddb0de40aa0a86a05fbd39971172
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-10-10 11:39:32 +0000

    Implementation of the Cart Router

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
+router.register('carts', views.CartViewSet)
 
 
 urlpatterns = router.urls
