9128c391bd3e3165b5128a8e2664885aa7d7b510
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-10-13 22:12:07 +0000

    Collections list per page changed to 12

--- store/admin.py
+++ store/admin.py
@@ -8,12 +8,16 @@ class BranchAdmin(admin.ModelAdmin):
     search_fields = ('name__istartswith',)
     ordering = ('name',)
     list_filter = ('is_active',)
+@admin.register(models.BranchAccount)
+class BranchAccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'branch')
+
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ("name",)
     list_filter = ('name',)
     search_fields = ('name__istartswith',)
-    list_per_page = 10
+    list_per_page = 12
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'is_available')
@@ -21,21 +25,55 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ('price', 'is_available')
     autocomplete_fields = ('collection',)
     search_fields = ('name__istartswith',)
+class OrderItemInline(admin.TabularInline):
+    model = models.OrderItem
+    min_num = 1
+    max_num = 10
+    extra = 0
+    autocomplete_fields = ('product',)
+
+
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('customer_name',
-                    'recipient_name',
-                    'recipient_number',
-                    'recipient_address',
-                    'status',
-                    )
+    list_display = (
+        'customer_name',
+        'recipient_name',
+        'recipient_number',
+        'recipient_address',
+        'status',
+        'branch',
+    )
     autocomplete_fields = ('branch',)
     list_filter = ('status',)
-    search_fields = ('customer_name__istartswith','recipient_name__istartswith',)
+    search_fields = ('customer_name__istartswith', 'recipient_name__istartswith',)
     list_editable = ('status',)
+    inlines = [OrderItemInline]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        try:
+            branch_account = models.BranchAccount.objects.get(user=request.user)
+            return qs.filter(branch=branch_account.branch)
+        except models.BranchAccount.DoesNotExist:
+            return qs.none()
+
+    def save_model(self, request, obj, form, change):
+        if not request.user.is_superuser:
+            try:
+                branch_account = models.BranchAccount.objects.get(user=request.user)
+                obj.branch = branch_account.branch
+            except models.BranchAccount.DoesNotExist:
+                pass
+        super().save_model(request, obj, form, change)
 
 
-@admin.register(models.OrderItem)
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ('product', 'quantity','price_at_purchase')
-    autocomplete_fields = ('product','order')
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser:
+            return ['branch']
+        return super().get_readonly_fields(request, obj)
+
+
+
