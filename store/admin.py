5693a1935111bc9ab873c79d8197f8d61a1851f3
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-10-22 14:41:12 +0000

    Registration of the Customer Admin

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.db.models import Count
+
 from . import models
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 # Register your models here.
 @admin.register(models.Branch)
 class BranchAdmin(admin.ModelAdmin):
@@ -77,3 +81,24 @@ class OrderAdmin(admin.ModelAdmin):
 
 
 
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'orders']
+    list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({
+                'customer__id': str(customer.id)
+            }))
+        return format_html('<a href="{}">{} Orders</a>', url, customer.orders_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
--- store/models.py
+++ store/models.py
@@ -29,7 +29,21 @@ class Product(models.Model):
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE, related_name='products')
     def __str__(self):
         return self.name
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True, blank=True)
+
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
 class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     STATUS_PENDING = 'Pending'
     STATUS_SHIPPED = 'Shipped'
     STATUS_COMPLETED = 'Completed'
@@ -80,19 +94,7 @@ class CartItem(models.Model):
     class Meta:
         unique_together = [['cart', 'product']]
 
-class Customer(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=255)
-    birth_date = models.DateField(null=True, blank=True)
 
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
-    class Meta:
-        ordering = ['first_name', 'last_name']
 
 
 
