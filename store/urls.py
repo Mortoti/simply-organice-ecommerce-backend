728a72e7bd77194b009594f95afb06d4b8eb4a48
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-11-03 13:15:13 +0000

    ProductImage URL Implemented

--- store/urls.py
+++ store/urls.py
@@ -1,15 +1,25 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework_nested import routers  # <-- Make sure 'include' is imported
 from . import views
-from rest_framework.routers import DefaultRouter
-from rest_framework_nested import routers
-router = DefaultRouter()
+
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
 router.register('carts', views.CartViewSet)
 router.register('customers', views.CustomerViewSet)
 router.register('orders', views.OrderViewSet, basename='orders')
+
+
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('images', views.ProductImageViewSet, basename='product-images')
+
+
 carts_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
 carts_router.register('items', views.CartItemViewSet, basename='cart-items')
 
-urlpatterns = router.urls + carts_router.urls
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(products_router.urls)),
+    path('', include(carts_router.urls)),
+]
