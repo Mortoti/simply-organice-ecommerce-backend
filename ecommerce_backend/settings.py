d9ca2ae02c71248fc4f662cf9bd61886dedd6f37
Mortoti-Agogyi Jephthah Lorlornyo <mortoti.dev@gmail.com>
2025-11-05 11:07:43 +0000

    Temporarily allowing all cors origin

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -166,11 +166,7 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ('JWT',),
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8001',
-    'http://127.0.0.1:8001'
-]
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
